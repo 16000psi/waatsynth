c861c220c704dd166131942acbe4e747385a63b6
16000psi <davidjhnsmith@gmail.com>
2024-03-11 19:40:13 +0000

    Refine TS

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// import { organ_2 } from '.wavetables/organ_2'
 class AudioManager {
   public audioContext: AudioContext;
 
@@ -15,15 +16,18 @@ class InputStage {
   public firstNode;
   public lastNode;
 
-  constructor(node, AudioManager) {
-    this.audioContext = AudioManager.audioContext;
-    this.audioElement = node.querySelector("audio") as HTMLMediaElement;
-    this.playButton = node.querySelector("[data-play-button]");
+  constructor(node: Element, audioManager: AudioManager) {
+    this.node = node;
+    this.audioContext = audioManager.audioContext;
+    this.audioElement = this.node.querySelector("audio") as HTMLMediaElement;
+    this.playButton = this.node.querySelector(
+      "[data-play-button]",
+    ) as HTMLElement;
     this.playButton.addEventListener("click", () => this.handlePlayButton());
     this.audioElement.addEventListener("ended", () => this.handleEnd());
     this.track = this.audioContext.createMediaElementSource(this.audioElement);
-    this.firstNode = this.track
-    this.lastNode = this.track
+    this.firstNode = this.track;
+    this.lastNode = this.track;
   }
 
   async handlePlayButton() {
@@ -42,16 +46,14 @@ class InputStage {
     this.playButton.dataset.playing = "false";
   }
 
-  connectBefore(targetModule) {
+  connectBefore(targetModule: OutputStage) {
     this.lastNode.connect(targetModule.firstNode);
   }
 }
 
 class OutputStage {
   private audioContext: AudioContext;
-  private audioElement: HTMLMediaElement;
   private node: Element;
-  private playButton: HTMLElement;
   private volumeControl: HTMLInputElement;
   private gainNode: GainNode;
   private panControl: HTMLInputElement;
@@ -59,37 +61,34 @@ class OutputStage {
   public firstNode;
   public lastNode;
 
-  constructor(node, AudioManager) {
-    this.node = node;
-    this.volumeControl = node.querySelector("[data-gain-control]");
-    this.panControl = node.querySelector("[data-pan-control]");
+  constructor(node: Element, audioManager: AudioManager) {
+    this.node = node as Element;
+    this.volumeControl = this.node.querySelector(
+      "[data-gain-control]",
+    ) as HTMLInputElement;
+    this.panControl = this.node.querySelector(
+      "[data-pan-control]",
+    ) as HTMLInputElement;
 
-    this.audioContext = AudioManager.audioContext;
+    this.audioContext = audioManager.audioContext;
     this.gainNode = this.audioContext.createGain();
     this.panNode = new StereoPannerNode(this.audioContext, { pan: 0 });
 
-    this.gainNode.connect(this.panNode)
+    this.gainNode.connect(this.panNode);
 
     this.volumeControl.addEventListener("input", () =>
       this.handleVolumeControl(),
     );
     this.panControl.addEventListener("input", () => this.handlePanControl());
-    this.firstNode = this.gainNode
-    this.lastNode = this.panNode
-  }
-
-  handleEnd() {
-    this.playButton.dataset.playing = "false";
+    this.firstNode = this.gainNode;
+    this.lastNode = this.panNode;
   }
 
   handleVolumeControl() {
-    this.gainNode.gain.value = this.volumeControl.value;
+    this.gainNode.gain.value = parseInt(this.volumeControl.value);
   }
   handlePanControl() {
-    this.panNode.pan.value = this.panControl.value;
-  }
-  connectBefore(targetModule) {
-    this.lastNode.connect(targetModule.firstNode);
+    this.panNode.pan.value = parseInt(this.panControl.value);
   }
   connectToDestination() {
     this.lastNode.connect(this.audioContext.destination);
@@ -98,11 +97,24 @@ class OutputStage {
 
 const master: AudioManager = new AudioManager();
 
-const inputElement: Element = document.querySelector("[data-audio-player]");
-const input: InputStage = new InputStage(inputElement, master);
+let input: InputStage | undefined;
+let output: OutputStage | undefined;
 
-const outputElement: Element = document.querySelector("[data-output-stage]");
-const output: OutputStage = new OutputStage(outputElement, master);
+const inputElement: Element | null = document.querySelector(
+  "[data-audio-player]",
+);
+if (inputElement) {
+  input = new InputStage(inputElement, master);
+}
 
-input.connectBefore(output)
-output.connectToDestination()
+const outputElement: Element | null = document.querySelector(
+  "[data-output-stage]",
+);
+if (outputElement) {
+  output = new OutputStage(outputElement, master);
+}
+
+if (input && output) {
+  input.connectBefore(output);
+  output.connectToDestination();
+}
